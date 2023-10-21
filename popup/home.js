15824c621c472ed39917b168fa8f54181622dd8d
Albert E <albertenrico.5@gmail.com>
2023-10-22 03:47:37 +0700

    Added function to show tab list in popup

--- popup/home.js
+++ popup/home.js
@@ -1,9 +1,39 @@
-chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-    document.write(`<h4>Tabs List:</h4>`);
-    document.write('<ul>');
+function getAllTabsList() {
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+        const tabListTitle = document.getElementById('tabListContainer');
+        const dupTabListTitle = document.getElementById('duplicateTabListContainer');
+        let urlTabs = getDuplicateTabsUrls(tabs).map((url) => {
+            return { url: url }
+        });
+        renderTabsList(tabs, tabListTitle);
+        renderTabsList(urlTabs, dupTabListTitle);
+    });
+}
+
+function getDuplicateTabsUrls(tabs) {
+    const tabsUrlCount = tabs.map((tab) => {
+        return { count: 1, tab: tab };
+    }).reduce((result, b) => {
+        result[b.tab.url] = (result[b.tab.url] || 0) + b.count;
+        return result;
+    }, {});
+    const duplicateUrls = Object.keys(tabsUrlCount).filter((a) => tabsUrlCount[a] > 1);
+
+    return duplicateUrls;
+}
+
+function renderTabsList(tabs, element) {
+    const ul = document.createElement('ul');
     let tabsLength = tabs.length;
     for (let i = 0; i < tabsLength; i++) {
-        document.write(`<li>${tabs[i].url}</li>`);
+        const li = document.createElement('li');
+        li.innerHTML = tabs[i].url;
+        ul.appendChild(li);
     }
-    document.write('</ul>');
-});
+    element.appendChild(ul);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const refreshListBtn = document.getElementById('refreshTabListBtn');
+    refreshListBtn.addEventListener('click', getAllTabsList);
+})
